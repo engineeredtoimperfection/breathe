c08e22991bca67d2eab259a3a7e7007ef7177b5a
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-05-13 08:50:20 +0530

    Animate font size

--- app/src/main/java/com/github/engineeredtoimperfection/breathe/MainActivity.kt
+++ app/src/main/java/com/github/engineeredtoimperfection/breathe/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.unit.sp
 import com.github.engineeredtoimperfection.breathe.ui.theme.BreatheTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +36,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             BreatheTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    val infiniteTransition = rememberInfiniteTransition()
+                    val fontSize by infiniteTransition.animateFloat(
+                        initialValue = 24F,
+                        targetValue = 36F,
+                        animationSpec = infiniteRepeatable(
+                            animation = tween(durationMillis = 4000, easing = LinearEasing),
+                            repeatMode = RepeatMode.Reverse
+                        )
+                    )
+
                     Box(modifier = Modifier
                         .padding(innerPadding)
                         .fillMaxSize()) {
                         Text(
                             text = "Breathe.",
-                            modifier = Modifier.align(Alignment.Center)
+                            modifier = Modifier.align(Alignment.Center),
+                            fontSize = fontSize.sp
                         )
                     }
                 }
