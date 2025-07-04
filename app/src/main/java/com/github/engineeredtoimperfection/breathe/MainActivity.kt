cddff57daecc4b9609a5fd6414247b6f315f9a9b
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-07-05 01:22:15 +0530

    Draw a still circle for the Pulsating Circle visualizer

--- app/src/main/java/com/github/engineeredtoimperfection/breathe/MainActivity.kt
+++ app/src/main/java/com/github/engineeredtoimperfection/breathe/MainActivity.kt
@@ -40,13 +40,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -238,10 +242,25 @@ fun PulsatingCircle(
     breathingTechnique: BreathingTechnique,
     toggleExploreMode: Modifier.() -> Modifier
 ) {
-    Text(
-        text = "PulsatingCircle visualizer not yet implemented.",
-        textAlign = TextAlign.Center,
+    Circle(modifier = modifier)
+}
+
+@Preview
+@Composable
+private fun Circle(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
+            .fillMaxSize()
+            .drawBehind {
+
+                val radius = size.width / 2.5f
+
+                drawCircle(
+                    color = Purple40,
+                    radius = radius,
+                    style = Stroke(width = 5f)
+                )
+            }
     )
 }
 
